a1cbc639f91ac5c2d951edf0c9279a14763688ee
ArturSHV <s.house-shop@yandex.ru>
2022-10-31 09:30:18 +0300

    исправлен путь к appsettings

--- WebApi/Helpers/SettingsHelpers.cs
+++ WebApi/Helpers/SettingsHelpers.cs
@@ -8,8 +8,7 @@ namespace WebApi.Helpers
         public static string? ReturnHostString()
         {
             string workingDirectory = Environment.CurrentDirectory;
-            string projectDirectory = Directory.GetParent(workingDirectory).Parent.Parent.FullName;
-            var filePath = Path.Combine(projectDirectory, "appsettings.json");
+            var filePath = Path.Combine(workingDirectory, "appsettings.json");
             string json = File.ReadAllText(filePath);
             var host = JObject.Parse(json)["Host"]?.Value<string>();
             return host;
